420f2353af7143b79929a3429b429cdcf4013ebb
James Austin <me@jamesaust.in>
2018-08-04 23:47:01 +1000

    More Maps

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -27,6 +27,32 @@ export default class MapScreen extends React.Component {
         containerElement={<div style={{ height: `100vh` }} />}
         mapElement={<div style={{ height: `100vh` }} />}
         coords={this.state}
+        jobs={[
+          {'name': 'Test 2', 
+           'description': 'Test 2',
+           'lat': -33.874166, 'lng': 151.203071, 
+           'completed': false,
+           'expensive': false
+          },
+          {'name': 'Test 1', 
+           'description': 'Find a place called testie mc test face',
+           'lat': -33.878166, 'lng': 151.204071,
+           'completed': true,
+           'expensive': true
+          },
+          {'name': 'Test 3', 
+           'description': 'Test 2',
+           'lat': -33.874166, 'lng': 151.204071, 
+           'completed': true,
+           'expensive': false
+          },
+          {'name': 'Test 4', 
+           'description': 'Find a place called testie mc test face',
+           'lat': -33.878166, 'lng': 151.203071,
+           'completed': false,
+           'expensive': true
+          },
+        ]}
       />
     )
   }
--- src/containers/MapComponent.js
+++ src/containers/MapComponent.js
@@ -5,13 +5,17 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
+  Marker, InfoWindow
 } from 'react-google-maps'
+
+var markerOk = require('../../public/google_maps_icon.png');
 
 class MapComponent extends React.Component {
   state = {
     didCenter: false,
-    coords: null
+    coords: null,
+    currentMarker: null,
+    selectedJob: {}
   }
 
   componentWillReceiveProps(nextProps) {
@@ -22,10 +26,51 @@ class MapComponent extends React.Component {
 
   componentDidMount() {
     // wew
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
   }
 
+  onMarkerClick(job, props, marker, e) {
+    this.setState({
+      selectedJob: job,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+  };
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
-    console.log(this.props)
+    const self = this;
+
+    const markers = this.props.jobs.map(function(job) {
+      return <Marker 
+          key={job.name}
+          job={job}
+          onClick={self.onMarkerClick.bind(this, job)}
+          icon={{url: job_icon(job.expensive, job.completed),
+                 scaledSize: new window.google.maps.Size(43, 43)}}
+          position={{lat: job.lat, lng: job.lng}}>
+                {
+           self.state.selectedJob == job && self.state.showingInfoWindow &&
+           <InfoWindow
+            key={'infowindow'}
+                visible={self.state.showingInfoWindow}>
+                  <div style={{width: '240px'}}>
+                    <h3>{self.state.selectedJob.name}</h3>
+                    <div>{self.state.selectedJob.description}</div>
+                  </div>
+              </InfoWindow>
+          }
+        </Marker>
+    });
     return (
       <GoogleMap
         defaultZoom={14}
@@ -37,13 +82,36 @@ class MapComponent extends React.Component {
         <Marker
           position={this.props.coords}
           defaultShape={'circle'}
+          icon={{url: markerOk}}
           shape={{
             coords: [this.props.coords.lat, this.props.coords.lng, 10],
             type: 'circle'
           }}
         />
+        {markers}
       </GoogleMap>
     )
+  }
+}
+
+var expensiveCompleteIcon = require('../../public/expensive_red.png');
+var expensiveIncompleteIcon = require('../../public/expensive_green.png');
+var cheapCompleteIcon = require('../../public/medium_red.png');
+var cheapIncompleteIcon = require('../../public/medium_green.png');
+
+var job_icon = function(expensive, completed) {
+  if (expensive) {
+    if (completed) {
+      return expensiveCompleteIcon;
+    } else {
+      return expensiveIncompleteIcon;
+    }
+  } else {
+    if (completed) {
+      return cheapCompleteIcon;
+    } else {
+      return cheapIncompleteIcon;
+    }
   }
 }
 
